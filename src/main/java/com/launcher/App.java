d5943241ee0a097001813c35928dae71625c503a
roberto miranda <rjmaltamar@gmail.com>
2011-07-21 19:19:03 -0500

    adding url parameter to the launcher

--- src/main/java/com/launcher/App.java
+++ src/main/java/com/launcher/App.java
@@ -8,7 +8,9 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,9 +19,15 @@ import java.util.logging.Logger;
  * Hello world!
  *
  */
-public class App 
-{   
-    public App (){
+public class App {
+
+    java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+    String root_url = "http://google.com";
+    String agua_url = "http://google.com";
+    String luz_url = "http://google.com";
+    String gas_url = "http://google.com";
+
+    public App() {
         try {
             Toolkit tk = Toolkit.getDefaultToolkit();
             URL url = getClass().getResource("/images/icon.gif");
@@ -27,10 +35,11 @@ public class App
             trayIcon.setToolTip("Energy");
             SystemTray tray = java.awt.SystemTray.getSystemTray();
             tray.add(trayIcon);
-            
+
             trayIcon.addActionListener(new ActionListener() {
+
                 public void actionPerformed(ActionEvent e) {
-                    
+                    launch(root_url);
                 }
             });
 
@@ -38,29 +47,33 @@ public class App
 
             MenuItem aguaItem = new java.awt.MenuItem("Agua");
             aguaItem.addActionListener(new ActionListener() {
+
                 public void actionPerformed(ActionEvent e) {
-                    
+                    launch(agua_url);
                 }
             });
 
             MenuItem luzItem = new java.awt.MenuItem("Luz");
             luzItem.addActionListener(new ActionListener() {
+
                 public void actionPerformed(ActionEvent e) {
-                    
+                    launch(luz_url);
                 }
             });
 
             MenuItem gasItem = new java.awt.MenuItem("Gas");
             gasItem.addActionListener(new ActionListener() {
+
                 public void actionPerformed(ActionEvent e) {
-                    
+                    launch(gas_url);
                 }
             });
-            
+
             MenuItem exitItem = new java.awt.MenuItem("Salir");
             exitItem.addActionListener(new ActionListener() {
+
                 public void actionPerformed(ActionEvent e) {
-                   System.exit(0); 
+                    System.exit(0);
                 }
             });
 
@@ -69,17 +82,27 @@ public class App
             trayMenu.add(gasItem);
             trayMenu.add(exitItem);
             trayIcon.setPopupMenu(trayMenu);
-            
-            
-            
-        }catch (AWTException ex) {
+
+
+
+        } catch (AWTException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void launch(String url) {
+        try {
+            java.net.URI uri = new java.net.URI(url);
+            desktop.browse(uri);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         new App();
-        
+
     }
 }
